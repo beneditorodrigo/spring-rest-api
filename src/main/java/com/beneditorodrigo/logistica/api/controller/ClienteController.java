72af69f6aae45d19f0e9cb2a856c2a92c818b745
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-07-20 23:43:11 -0300

    Persistencia com Jakarta EE

--- src/main/java/com/beneditorodrigo/logistica/api/controller/ClienteController.java
+++ src/main/java/com/beneditorodrigo/logistica/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.beneditorodrigo.logistica.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,13 @@ import com.beneditorodrigo.logistica.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Benedito Rodrigo de Sousa Cunha");
-		cliente1.setTelefone("99 98888-3333");
-		cliente1.setEmail("rodrigodacunha7@gmail.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("HyperTech Tecnologia");
-		cliente2.setTelefone("88 93333-1111");
-		cliente2.setEmail("aghypertech@gmail.com");
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 	
 }
--- src/main/java/com/beneditorodrigo/logistica/domain/model/Cliente.java
+++ src/main/java/com/beneditorodrigo/logistica/domain/model/Cliente.java
@@ -1,10 +1,24 @@
 package com.beneditorodrigo.logistica.domain.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Cliente {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
 	private String nome;
 	private String email;
+
+	@Column(name = "fone")
 	private String telefone;
 
 	public Long getId() {
@@ -37,5 +51,22 @@ public class Cliente {
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
 	}
 }
