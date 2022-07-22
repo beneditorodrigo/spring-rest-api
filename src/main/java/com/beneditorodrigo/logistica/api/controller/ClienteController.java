2f251e9a3f2e9a60bd4de7cf13b9a16c4b9c6686
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-07-21 22:33:04 -0300

    Método para adcionar o registro de um cliente na base de dados (BD)

--- src/main/java/com/beneditorodrigo/logistica/api/controller/ClienteController.java
+++ src/main/java/com/beneditorodrigo/logistica/api/controller/ClienteController.java
@@ -1,24 +1,29 @@
 package com.beneditorodrigo.logistica.api.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beneditorodrigo.logistica.domain.model.Cliente;
 import com.beneditorodrigo.logistica.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
 	
-	@GetMapping("/clientes")
+	@GetMapping()
 	public List<Cliente> listar() {
 		
 		return clienteRepository.findAll();
@@ -26,10 +31,16 @@ public class ClienteController {
 		//return clienteRepository.findByNomeContaining("Hy");
 	}
 	
-	@GetMapping("/clientes/{clienteId}")
+	@GetMapping("/{clienteId}")
 	public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
 		return clienteRepository.findById(clienteId)
 				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping()
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adcionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 }
