cca537aad5c42eedd26ff18d143dbc535b1f6a6a
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-07-26 23:34:42 -0300

    Isolando Domain Model do Representation Model no método de buscar Entrega por ID

--- src/main/java/com/beneditorodrigo/logistica/api/controller/EntregaController.java
+++ src/main/java/com/beneditorodrigo/logistica/api/controller/EntregaController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.beneditorodrigo.logistica.api.model.DestinatarioModel;
+import com.beneditorodrigo.logistica.api.model.EntregaModel;
 import com.beneditorodrigo.logistica.domain.model.Entrega;
 import com.beneditorodrigo.logistica.domain.repository.EntregaRepository;
 import com.beneditorodrigo.logistica.domain.service.SolicitacaoEntregaService;
@@ -38,9 +40,25 @@ public class EntregaController {
 	}
 	
 	@GetMapping("/{entregaId}")
-	public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId){
+	public ResponseEntity<EntregaModel> buscar(@PathVariable Long entregaId){
 		return entregaRepository.findById(entregaId)
-				.map(ResponseEntity::ok)
-				.orElse(ResponseEntity.notFound().build());
+				.map(entrega -> {
+					EntregaModel entregaModel = new EntregaModel();
+					
+					entregaModel.setId(entrega.getId());
+					entregaModel.setNomeCliente(entrega.getCliente().getNome());
+					entregaModel.setDestinatario(new DestinatarioModel());
+					entregaModel.getDestinatario().setNome(entrega.getDestinatario().getNome());
+					entregaModel.getDestinatario().setLogradouro(entrega.getDestinatario().getLogradouro());
+					entregaModel.getDestinatario().setNumero(entrega.getDestinatario().getNumero());
+					entregaModel.getDestinatario().setComplemento(entrega.getDestinatario().getComplemento());
+					entregaModel.getDestinatario().setBairro(entrega.getDestinatario().getBairro());
+					entregaModel.setTaxa(entrega.getTaxa());
+					entregaModel.setStatus(entrega.getStatus());
+					entregaModel.setDataPedido(entrega.getDataPedido());
+					entregaModel.setDataFinalizacao(entrega.getDataFinalizacao());
+					
+					return ResponseEntity.ok(entregaModel);
+				}).orElse(ResponseEntity.notFound().build());
 	}
 }
