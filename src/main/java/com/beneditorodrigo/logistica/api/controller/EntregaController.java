2f27c6ad8d1528df78789679ff57609725d3e34a
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-07-26 21:42:52 -0300

    Fazendo validação em cascata com ValidationGroup

--- src/main/java/com/beneditorodrigo/logistica/api/controller/EntregaController.java
+++ src/main/java/com/beneditorodrigo/logistica/api/controller/EntregaController.java
@@ -1,12 +1,20 @@
 package com.beneditorodrigo.logistica.api.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beneditorodrigo.logistica.domain.model.Entrega;
+import com.beneditorodrigo.logistica.domain.repository.EntregaRepository;
 import com.beneditorodrigo.logistica.domain.service.SolicitacaoEntregaService;
 
 @RestController
@@ -14,10 +22,25 @@ import com.beneditorodrigo.logistica.domain.service.SolicitacaoEntregaService;
 public class EntregaController {
 
 	@Autowired
-	SolicitacaoEntregaService solicitacaoEntregaService;
+	private EntregaRepository entregaRepository;
+	
+	@Autowired
+	private SolicitacaoEntregaService solicitacaoEntregaService;
 	
 	@PostMapping
-	public Entrega solicita(@RequestBody Entrega entrega) {
+	public Entrega solicita(@Valid @RequestBody Entrega entrega) {
 		return solicitacaoEntregaService.solicitar(entrega);
+	}
+	
+	@GetMapping
+	public List<Entrega> listar(){
+		return entregaRepository.findAll();
+	}
+	
+	@GetMapping("/{entregaId}")
+	public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId){
+		return entregaRepository.findById(entregaId)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
---
+++ src/main/java/com/beneditorodrigo/logistica/domain/ValidationGroups.java
@@ -0,0 +1,6 @@
+package com.beneditorodrigo.logistica.domain;
+
+public interface ValidationGroups {
+
+	public interface ClienteId{}
+}
